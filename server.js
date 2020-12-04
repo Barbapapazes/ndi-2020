fdb1b497b7b018e9c1e829c36434339cf50a903e
abarry <amadou_tidiane.barry.insa-cvl.fr>
2020-12-04 03:55:22 +0100

    deletes

--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
-
-const port = process.env.PORT || 4000;
-
-server.listen(port);
